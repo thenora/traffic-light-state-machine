e31c97b8523ba2b986cc8cbbc7047acb3ea19cd9
thenora <thenora@gmail.com>
2020-05-18 18:31:51 -0700

    wave 1 - implemented state machine blueprint with xstate

--- src/state-machines/LightMachine.js
+++ src/state-machines/LightMachine.js
@@ -2,7 +2,12 @@ import { createMachine, interpret } from 'xstate';
 
 const lightMachine = createMachine({
   // Your state machine blueprint here
-
+  id: 'trafficLight',
+  initial: 'red',
+  states: {
+    red: { on: { changeLight: 'green' } },
+    green: { on: { changeLight: 'yellow' } },
+    yellow: { on: { changeLight: 'red' } }
 });
 
 
